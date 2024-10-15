55ab552dadeddd1413701c4fa463759512308afc
bytesculptors <tuannghia200603@gmail.com>
2024-10-15 20:51:30 +0700

    Handle answer question and calculate score

--- src/main/java/com/kirito/quizapp/controllers/QuizController.java
+++ src/main/java/com/kirito/quizapp/controllers/QuizController.java
@@ -1,14 +1,20 @@
 package com.kirito.quizapp.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kirito.quizapp.models.QuestionWrapper;
 import com.kirito.quizapp.models.Quiz;
+import com.kirito.quizapp.models.Response;
 import com.kirito.quizapp.services.QuizService;
 
 @RestController
@@ -21,5 +27,15 @@ public class QuizController {
   @PostMapping
   public ResponseEntity<Quiz> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
     return quizService.createQuiz(category, numQ, title);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+    return quizService.getQuizQuestions(id);
+  }
+
+  @PostMapping("submit/{id}")
+  public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> response) {
+    return quizService.calculateScore(id, response);
   }
 }
--- src/main/java/com/kirito/quizapp/services/QuizService.java
+++ src/main/java/com/kirito/quizapp/services/QuizService.java
@@ -1,6 +1,8 @@
 package com.kirito.quizapp.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +12,9 @@ import org.springframework.stereotype.Service;
 import com.kirito.quizapp.dao.QuestionDao;
 import com.kirito.quizapp.dao.QuizDao;
 import com.kirito.quizapp.models.Question;
+import com.kirito.quizapp.models.QuestionWrapper;
 import com.kirito.quizapp.models.Quiz;
+import com.kirito.quizapp.models.Response;
 
 @Service
 public class QuizService {
@@ -28,5 +32,25 @@ public class QuizService {
     return new ResponseEntity<>(quiz, HttpStatus.CREATED);
   }
 
+  public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+    Optional<Quiz> quiz = quizDao.findById(id);
+    List<Question> questionsFromDB = quiz.get().getQuestions();
+    List<QuestionWrapper> questionForUser = new ArrayList<>();
+    for (Question q : questionsFromDB) {
+      QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+      questionForUser.add(qw);
+    }
+    return new ResponseEntity<>(questionForUser, HttpStatus.OK);
+  }
+
+  public ResponseEntity<Integer> calculateScore(Integer id, List<Response> responses) {
+    Quiz quiz = quizDao.findById(id).get();
+    List<Question> questions = quiz.getQuestions();
+    int right = 0;
+    for (int i = 0; i < responses.size(); i++) {
+      if (responses.get(i).getResponse().equals(questions.get(i).getRightAnswer())) right++;
+    }
+    return new ResponseEntity<>(right, HttpStatus.OK);
+  }
    
 }
